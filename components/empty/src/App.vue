d0720c1861eb94e82c547748b466295000e9d29d
wm <531431988@qq.com>
2019-05-30 16:34:15 +0800

    添加空状态组件

---
+++ components/empty/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <Row :gutter="24">
+      <Col span="12" class="mb15">
+        <Card title="title方式">
+          <Empty title="暂无数据"/>
+        </Card>
+      </Col>
+      <Col span="12" class="mb15">
+        <Card title="slot方式">
+          <Empty>
+            <template slot="text">暂无数据</template>
+          </Empty>
+        </Card>
+      </Col>
+      <Col span="12" class="mb15">
+        <Card title="简洁版">
+          <Empty type="2">
+            <template slot="text">暂无数据</template>
+          </Empty>
+        </Card>
+      </Col>
+      <Col span="12" class="mb15">
+        <Card title="自定义图片">
+          <Empty>
+            <Icon type="ios-happy-outline" size="48"/>
+            <template slot="text">自定义文字提示</template>
+          </Empty>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+</template>
+          </Empty>
+        </Card>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+import Empty from '_c/Empty'
+export default {
+  components: {
+    Empty
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 5% 20%;
+}
+.mb15 {
+  margin-bottom: 15px;
+}
+</style>
---
+++ components/empty/src/components/Empty.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="vue-empty">
+    <div v-if="$slots.default">
+      <slot></slot>
+    </div>
+    <div v-else>
+      <img :src="src">
+    </div>
+    <div v-if="$slots.text">
+      <slot name="text"></slot>
+    </div>
+    <div v-else>{{title}}</div>
+  </div>
+</template>
+
+<script>
+import emptyDefault from '@/assets/default.png'
+import emptySimple from '@/assets/simple.png'
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '暂无数据'
+    },
+    type: {
+      type: String,
+      default: '1'
+    }
+  },
+  data () {
+    return {
+      src: this.type === '1' ? emptyDefault : emptySimple
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.vue-empty {
+  text-align: center;
+  color: @text-color;
+}
+</style>
