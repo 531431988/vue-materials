3ebd27d7c2a4ee79b5cee83e84e52504c160e0cd
wm <531431988@qq.com>
2019-06-03 14:18:23 +0800

    清理文件

--- components/click-outside/README.md
+++
@@ -1,7 +0,0 @@
-# click-outside
-
-简介：全局注册指令。
-
----
-
-![截图](https://531431988.github.io/vue-component-library/components/click-outside/thumbnail.png)
--- components/click-outside/src/directive/index.js
+++
@@ -1,7 +0,0 @@
-import clickOutside from './click-outside'
-
-const importDirective = Vue => {
-  Vue.directive(clickOutside.name, clickOutside)
-}
-
-export default importDirective
