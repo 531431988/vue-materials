d06c5688ba484fcb3c78967b847e1294d7a4b977
wm <531431988@qq.com>
2019-06-04 10:18:25 +0800

    更新v-click-outside

--- components/v-click-outside/src/main.js
+++ components/v-click-outside/src/main.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import iView from 'iview'
-import importDirective from '@/directive'
+import * as vClickOutside from 'v-click-outside-x'
 import './theme.less'
 Vue.use(iView)
+Vue.use(vClickOutside)
 Vue.config.productionTip = false
-/**
- * 注册指令
- */
-importDirective(Vue)
 new Vue({
   render: h => h(App)
 }).$mount('#app')
