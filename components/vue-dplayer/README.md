9f836d6c5ffb91595ab2c248bcbbbf7717bdaaaf
wm <531431988@qq.com>
2019-06-04 16:17:04 +0800

    添加vue-dplayer

---
+++ components/vue-dplayer/README.md
@@ -0,0 +1,7 @@
+# vue-dplayer
+
+简介：视频播放器。
+
+---
+
+![截图](https://531431988.github.io/vue-component-library/components/vue-dplayer/thumbnail.png)
---
+++ components/vue-dplayer/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <Card title="dplayer">
+      <d-player :options="options" @play="play" ref="player"></d-player>
+      <Button slot="extra" type="primary" @click="onSwitch">切换视频</Button>
+    </Card>
+  </div>
+</template>
+
+<script>
+import VueDPlayer from 'vue-dplayer'
+import 'vue-dplayer/dist/vue-dplayer.css'
+export default {
+  components: {
+    'd-player': VueDPlayer
+  },
+  data () {
+    return {
+      options: {
+        video: {
+          url: 'http://static.smartisanos.cn/common/video/t1-ui.mp4',
+          pic: 'http://static.smartisanos.cn/pr/img/video/video_03_cc87ce5bdb.jpg'
+        },
+        autoplay: true,
+        contextmenu: []
+      },
+      player: null
+    }
+  },
+  mounted () {
+    this.player = this.$refs.player.dp
+  },
+  methods: {
+    play () {
+      console.log('play callback')
+    },
+    onSwitch () {
+      this.player.switchVideo({
+        url: 'http://static.smartisanos.cn/common/video/video-jgpro.mp4'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  margin: 5% 20%;
+  .dplayer-menu {
+    .dplayer-menu-item {
+      &:not(:first-child) {
+        display: none !important;
+      }
+    }
+  }
+}
+</style>
