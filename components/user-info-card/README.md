9ad7d3ccf917ea2f71c7c06536fb8582eeb3a0b8
wm <531431988@qq.com>
2019-06-05 16:56:53 +0800

    清除user-info-card组件

--- components/user-info-card/README.md
+++
@@ -1,7 +0,0 @@
-# user-info-card
-
-简介：用于展示用户信息的卡片。
-
----
-
-![截图](https://531431988.github.io/vue-component-library/components/user-info-card/thumbnail.png)
--- components/user-info-card/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import iView from 'iview'
-import './theme.less'
-Vue.use(iView)
-Vue.config.productionTip = false
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
