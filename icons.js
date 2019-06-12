af5b09306bf1d94a3698299ba978ddd16027c541
wm <531431988@qq.com>
2019-06-12 10:32:11 +0800

    更新图标

--- icons.js
+++ icons.js
@@ -1,6 +1,7 @@
 // export what you need
 export { default as SmileOutline } from '@ant-design/icons/lib/outline/SmileOutline'
 export { default as MehOutline } from '@ant-design/icons/lib/outline/MehOutline'
+export { default as GithubOutline } from '@ant-design/icons/lib/outline/GithubOutline'
 
 // export what antd other components need
 export { default as CloseOutline } from '@ant-design/icons/lib/outline/CloseOutline'
