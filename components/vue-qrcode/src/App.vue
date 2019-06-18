f12866af259e8873ffaaa1c7f5532cdfe1777804
wm <531431988@qq.com>
2019-06-18 16:12:33 +0800

    添加vue-qrcode组件

---
+++ components/vue-qrcode/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Row type="flex" justify="center" style="padding-top:50px">
+    <Col span="6">
+      <h2>
+        <a href="https://fengyuanchen.github.io/vue-qrcode/">vue-qrcode api</a>
+      </h2>
+      <vue-qrcode value="https://fengyuanchen.github.io/vue-qrcode/"></vue-qrcode>
+      <vue-qrcode
+        value="https://fengyuanchen.github.io/vue-qrcode/"
+        :options="{ color: { dark: '#0275d8' } }"
+      ></vue-qrcode>
+      <vue-qrcode value="https://fengyuanchen.github.io/vue-qrcode/" tag="img"></vue-qrcode>
+    </Col>
+  </Row>
+</template>
+
+<script>
+import vueQrcode from '@chenfengyuan/vue-qrcode'
+export default {
+  components: {
+    vueQrcode
+  }
+}
+</script>
+
+<style>
+#app {
+  padding: 5% 20%;
+}
+</style>
---
+++ components/vue-qrcode/src/plugins/iview.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import { Row, Col, Card, Button, Icon } from 'iview'
+import '../theme.less'
+const component = {
+  Row,
+  Col,
+  Card,
+  Button,
+  Icon
+}
+Object.keys(component).forEach(key => {
+  Vue.component(key, component[key])
+})
