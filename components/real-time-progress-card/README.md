146f7959553591e645227bc12d570ca280da33c1
wm <531431988@qq.com>
2019-06-19 11:20:25 +0800

    添加 实时进度卡片信息总览 组件

---
+++ components/real-time-progress-card/README.md
@@ -0,0 +1,7 @@
+# real-time-progress-card
+
+简介：实时进度卡片信息总览
+
+---
+
+![截图](https://531431988.github.io/vue-component-library/components/real-time-progress-card/thumbnail.png)
---
+++ components/real-time-progress-card/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <Row type="flex" justify="center" style="padding-top:50px">
+    <Col span="18">
+      <Row :gutter="24">
+        <Col span="6" v-for="(item, index) in data" :key="index">
+          <real-time-progress-card :data="item"></real-time-progress-card>
+        </Col>
+      </Row>
+    </Col>
+  </Row>
+</template>
+
+<script>
+import RealTimeProgressCard from '_c/RealTimeProgressCard'
+export default {
+  components: {
+    RealTimeProgressCard
+  },
+  data () {
+    return {
+      data: [{
+        title: '实时下单数（单）',
+        total: 1602,
+        contrast: true,
+        percent: 54,
+        color: '#2d8cf0'
+      }, {
+        title: '实时浏览量（次）',
+        total: 9698,
+        contrast: false,
+        percent: 76,
+        color: '#19be6b'
+      }, {
+        title: '今日交易量（元）',
+        total: 62608,
+        contrast: false,
+        percent: 33,
+        color: '#ff9900'
+      }, {
+        title: '今日关注量（人）',
+        total: 2936,
+        contrast: true,
+        percent: 80,
+        color: '#ed4014'
+      }]
+    }
+  }
+}
+</script>
+
+
